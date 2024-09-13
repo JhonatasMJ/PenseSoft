cd64f728f25703a0c1d3a40b2a5bd6f60918418e
Daniel <danieeldesoares@hotmail.com>
2024-09-13 17:35:17 -0300

    Form Update / Nav Update / Footer Update

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,121 +1,123 @@
 // src/Components/Navbar.js
-import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
-import logo from "../assets/imgs/logotipo.svg";
-import { Link as ScrollLink } from "react-scroll";
-import { Menu, X } from "lucide-react";
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '../assets/imgs/logotipo.svg';
+import { Link as ScrollLink } from 'react-scroll';
+import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
-  const location = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
+	const location = useLocation();
+	const [isOpen, setIsOpen] = useState(false);
 
-  const isHomePage = location.pathname === "/";
+	const isHomePage = location.pathname === '/';
 
-  const toggleMenu = () => setIsOpen(!isOpen);
+	const toggleMenu = () => setIsOpen(!isOpen);
 
-  return (
-    <div className="bg-ciano">
-      <header className="flex items-center justify-between max-w-7xl mx-auto p-4">
-        <img src={logo} alt="Logotipo" className="h-10" />
+	return (
+		<div className="bg-ciano fixed top-0 left-0 w-full z-50">
+			<header className="flex items-center justify-between max-w-7xl mx-auto p-4">
+				<img src={logo} alt="Logotipo" className="h-10" />
 
-        {/* Ícone do menu para dispositivos móveis */}
-        <div className="lg:hidden flex items-center">
-          <button
-            onClick={toggleMenu}
-            className={`text-white transition-transform duration-300 ${isOpen ? 'rotate-90' : ''}`}
-          >
-            {isOpen ? <X size={32} /> : <Menu size={32} />}
-          </button>
-        </div>
+				{/* Ícone do menu para dispositivos móveis */}
+				<div className="lg:hidden flex items-center">
+					<button
+						onClick={toggleMenu}
+						className={`text-white transition-transform duration-300 ${
+							isOpen ? 'rotate-90' : ''
+						}`}
+					>
+						{isOpen ? <X size={32} /> : <Menu size={32} />}
+					</button>
+				</div>
 
-        {/* Menu de navegação */}
-        <nav
-          className={`${
-            isOpen ? "block" : "hidden"
-          } lg:flex flex-1 justify-center items-center text-white gap-6 font-semibold text-lg lg:static absolute top-[70px] left-0 w-full lg:w-auto lg:bg-transparent bg-ciano transition-all duration-300 ease-in-out`}
-          style={{ zIndex: isOpen ? 40 : 10 }}
-        >
-          <ul className="flex flex-col lg:flex-row gap-6 lg:items-center text-center lg:text-left p-4 lg:p-0">
-            {isHomePage ? (
-              <>
-                <li className="cursor-pointer">
-                  <ScrollLink
-                    to="inicio"
-                    smooth={true}
-                    duration={500}
-                    onClick={toggleMenu}
-                  >
-                    Inicio
-                  </ScrollLink>
-                </li>
-                <li className="cursor-pointer">
-                  <ScrollLink
-                    to="sobre"
-                    smooth={true}
-                    duration={500}
-                    onClick={toggleMenu}
-                  >
-                    Sobre
-                  </ScrollLink>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link
-                    to="/"
-                    state={{ scrollTo: "inicio" }}
-                    onClick={toggleMenu}
-                  >
-                    Inicio
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/"
-                    state={{ scrollTo: "sobre" }}
-                    onClick={toggleMenu}
-                  >
-                    Sobre
-                  </Link>
-                </li>
-              </>
-            )}
-            <li>
-              <Link to="/funcionalidades" onClick={toggleMenu}>
-                Funcionalidades
-              </Link>
-            </li>
-            <li className="cursor-pointer">
-              <ScrollLink
-                to="vantagens"
-                smooth={true}
-                duration={500}
-                onClick={toggleMenu}
-              >
-                Vantagens
-              </ScrollLink>
-            </li>
-            {/* Botão Orçamento dentro do menu hambúrguer */}
-            <li className="lg:hidden mt-4">
-              <Link to="/orcamento">
-                <button className="bg-azul text-white font-bold px-4 py-2 rounded">
-                  Orçamento
-                </button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+				{/* Menu de navegação */}
+				<nav
+					className={`${
+						isOpen ? 'block' : 'hidden'
+					} lg:flex flex-1 justify-center items-center text-white gap-6 font-semibold text-lg lg:static absolute top-[70px] left-0 w-full lg:w-auto lg:bg-transparent bg-ciano transition-all duration-300 ease-in-out`}
+					style={{ zIndex: isOpen ? 40 : 10 }}
+				>
+					<ul className="flex flex-col lg:flex-row gap-6 lg:items-center text-center lg:text-left p-4 lg:p-0">
+						{isHomePage ? (
+							<>
+								<li className="cursor-pointer">
+									<ScrollLink
+										to="inicio"
+										smooth={true}
+										duration={500}
+										onClick={toggleMenu}
+									>
+										Inicio
+									</ScrollLink>
+								</li>
+								<li className="cursor-pointer">
+									<ScrollLink
+										to="sobre"
+										smooth={true}
+										duration={500}
+										onClick={toggleMenu}
+									>
+										Sobre
+									</ScrollLink>
+								</li>
+							</>
+						) : (
+							<>
+								<li>
+									<Link
+										to="/"
+										state={{ scrollTo: 'inicio' }}
+										onClick={toggleMenu}
+									>
+										Inicio
+									</Link>
+								</li>
+								<li>
+									<Link
+										to="/"
+										state={{ scrollTo: 'sobre' }}
+										onClick={toggleMenu}
+									>
+										Sobre
+									</Link>
+								</li>
+							</>
+						)}
+						<li>
+							<Link to="/funcionalidades" onClick={toggleMenu}>
+								Funcionalidades
+							</Link>
+						</li>
+						<li className="cursor-pointer">
+							<ScrollLink
+								to="vantagens"
+								smooth={true}
+								duration={500}
+								onClick={toggleMenu}
+							>
+								Vantagens
+							</ScrollLink>
+						</li>
+						{/* Botão Orçamento dentro do menu hambúrguer */}
+						<li className="lg:hidden mt-4">
+							<Link to="/orcamento">
+								<button className="bg-azul text-white font-bold px-4 py-2 rounded">
+									Orçamento
+								</button>
+							</Link>
+						</li>
+					</ul>
+				</nav>
 
-        {/* Botão Orçamento para dispositivos grandes */}
-        <Link to="/orcamento" className="hidden lg:block">
-          <button className="bg-azul text-white font-bold px-4 py-2 rounded">
-            Orçamento
-          </button>
-        </Link>
-      </header>
-    </div>
-  );
+				{/* Botão Orçamento para dispositivos grandes */}
+				<Link to="/orcamento" className="hidden lg:block">
+					<button className="bg-azul text-white font-bold px-4 py-2 rounded transition-all duration-300 ease-in hover:scale-110 hover:bg-azul-hover">
+						Orçamento
+					</button>
+				</Link>
+			</header>
+		</div>
+	);
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Libre Franklin"', 'sans-serif'], 
-      },
-      colors: {
-        'ciano': '#21B2D8',
-        'ciano-claro': '#0BA9D2',
-        'azul': '#2E5077',
-        'cinza': '#3A3A3A',
-        'azul-escuro': '#2e5077',
-      },
-      backgroundImage: {
-        'teste': "url('./assets/imgs/womanHome.png')",
-      },
-      boxShadow: {
-        'custom-ciano': '0 8px 20px -1px rgba(33, 178, 216, 0.5)',
-      },
-      inset: {
-        '48p': '48%',
-        '49p': '49%',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Libre Franklin"', 'sans-serif'],
+			},
+			colors: {
+				ciano: '#21B2D8',
+				'ciano-claro': '#0BA9D2',
+				azul: '#2E5077',
+				cinza: '#3A3A3A',
+				'azul-escuro': '#2e5077',
+				'azul-hover': '#1d3148',
+			},
+			backgroundImage: {
+				teste: "url('./assets/imgs/womanHome.png')",
+			},
+			boxShadow: {
+				'custom-ciano': '0 8px 20px -1px rgba(33, 178, 216, 0.5)',
+			},
+			inset: {
+				'48p': '48%',
+				'49p': '49%',
+			},
+		},
+	},
+	plugins: [],
+};
