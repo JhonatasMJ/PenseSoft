91fc89175f08a0601d805687965bf9a1d88bb196
JHONATAS  MICAEL JACOMETTO <0220832311009@fatectq.local>
2024-09-30 09:12:53 -0300

    Remoção Animação

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 // src/App.js
-import { useState } from "react";
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Main from "./Pages/Main";
 import Orcamento from './Pages/Orcamento';
-import Splash from './Components/Splash';
+
 
 const App = () => {
-  const [showSplash, setShowSplash] = useState(true);
-
-  const handleAnimationComplete = () => {
-    setShowSplash(false);
-  };
 
   return (
     <Router>
-      {showSplash ? (
-        <Splash onAnimationComplete={handleAnimationComplete} />
-      ) : (
+
         <Routes>
           <Route
             path="/"
@@ -38,7 +32,7 @@ const App = () => {
             }
           />
         </Routes>
-      )}
+
     </Router>
   );
 };
